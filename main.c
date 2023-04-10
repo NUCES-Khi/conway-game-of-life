1e50659304655753364ea58c4038522433c55cce
Rayyan3079 <124567636+Rayyan3079@users.noreply.github.com>
2023-04-10 11:37:18 +0500

    Update main.c

--- main.c
+++ main.c
@@ -48,10 +48,80 @@ int count_neighbors(int board[][10], int row, int col) {
     }
     return count;
 }
+// This function uses count_neighbors functions to count the neighbour of a particular cell and apply the needed operation to it
+void update_board(int board[][COLS], int rows, int cols) {
+    int new_board[ROWS][COLS];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            int neighbors = count_neighbors(board, i, j);
+            if (board[i][j] == 1 && (neighbors < 2 || neighbors > 3)) {
+                new_board[i][j] = 0;
+            } else if (board[i][j] == 0 && neighbors == 3) {
+                new_board[i][j] = 1;
+            } else {
+                new_board[i][j] = board[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            board[i][j] = new_board[i][j];
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    // It is left up to you how you use shared memory and multiple processes 
-    // to solve the conway problem
-    return 0;
+	//share memory
+    int shmid;
+    key_t key = IPC_PRIVATE;
+    int (*board)[COLS];
+	// creating shared memory
+    shmid = shmget(key, ROWS * COLS * sizeof(int), IPC_CREAT | 0666);
+    if (shmid < 0) {
+        perror("shmget");
+        exit(1);
+    }
+	// assigning memory block to it
+    board = shmat(shmid, NULL, 0);
+    if (board == (int (*)[]) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    // Initialize the board
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            board[i][j] = rand() % 2;
+        }
+    }
+
+    int pid;
+    int status;
+
+    for (int i = 0; i < 10; i++) {
+        pid = fork();
+        if (pid == 0) {
+            int start_row = i * ROWS / 10;
+            int end_row = (i + 1) * ROWS / 10;
+            for (int j = 0; j < 100; j++) {
+            update_board(board, end_row - start_row, COLS);
+        }
+        exit(0);
+    } else if (pid < 0) {
+        perror("fork");
+        exit(1);
+    }
+}
+
+for (int i = 0; i < 10; i++) {
+    wait(&status);
+}
+
+print_board(board, ROWS, COLS);
+
+shmdt(board);
+shmctl(shmid, IPC_RMID, NULL);
+
+return 0;
 }
